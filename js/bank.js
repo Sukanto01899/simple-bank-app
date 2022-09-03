3ba919087dd34e18b9639c945c70ec27e87f6a61
Sukanto Das <Suknto01899>
2022-09-04 00:54:45 +0600

    code edited

--- js/bank.js
+++ js/bank.js
@@ -1,88 +1,66 @@
-//Deposite
-let inputDeposit = document.getElementById('input-deposit');
-let depositedBalance = document.getElementById('deposit-balance');
-let mainBalance = document.getElementById('main-balance');
-let tableRow = document.querySelector('tbody');
-let withdrawBtn = document.getElementById('withdraw-btn');
-let inputWithdraw = document.getElementById('input-withdraw');
-let withdrawBalance = document.getElementById('withdraw-balance')
+const depositBtn = document.getElementById('deposit-btn');
+const withdrawBtn = document.getElementById('withdraw-btn');
+let inputDepositText = document.getElementById('input-deposit');
+let inputWithdrawText = document.getElementById('input-withdraw');
+let mainBalanceText = document.getElementById('main-balance');
+let depositBalanceText = document.getElementById('deposit-balance');
+let withdrawBalanceText = document.getElementById('withdraw-balance');
 
-document.getElementById('deposit-btn').addEventListener('click', (e)=>{
-    let depositedBalanceText = parseFloat(depositedBalance.innerText);
-    let inputDepositValue = parseFloat(inputDeposit.value);
-    let mainBalanceText = parseFloat(mainBalance.innerText);
-    
-    if(isNaN(inputDeposit.value) || inputDeposit.value === ''){
-        document.getElementById('warning').innerText = "Please input a vaild number"
+function textToNumber(input){
+    let inputTextToNumber = parseFloat(input);
+    return inputTextToNumber;
+}
+
+function updateBalance(inputAmount, balanceAmount, addBlance){
+    let mainBalanceNumber = textToNumber(mainBalanceText.innerText);   
+    if(addBlance === true){
+        depositBalanceText.innerText = inputAmount + balanceAmount;
+        mainBalanceText.innerText = mainBalanceNumber + inputAmount;
     }else{
-        document.getElementById('warning').innerText = "";
-        //Add Balance on Deposit Section
-    let totalDeposite = depositedBalanceText + inputDepositValue;
-    depositedBalance.innerText = totalDeposite.toFixed(2);
-    //Add Balance on Total Balance Section
-    let mainBalanceTotal = mainBalanceText + inputDepositValue;
-    mainBalance.innerText = mainBalanceTotal.toFixed(2);
+        withdrawBalanceText.innerText = inputAmount + balanceAmount;
+        mainBalanceText.innerText = mainBalanceNumber - inputAmount;
+    }
+}
 
+function addTable(about, amount){
+    let trParent = document.querySelector("tbody");
     let tr = document.createElement('tr');
     let td1 = document.createElement('td');
-    td1.innerText = 'Deposit'
+    td1.innerText = about;
     let td2 = document.createElement('td');
-    td2.innerText = 'Success';
+    td2.innerText = 'success';
     td2.setAttribute('class', 'status')
     let td3 = document.createElement('td');
-    td3.innerText = `+ $${totalDeposite.toFixed(2)}`;
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
+    td3.innerText = amount;    
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+    tr.appendChild(td3)
+    trParent.appendChild(tr);
+}
 
-    tableRow.appendChild(tr);
-
-
-    inputDeposit.value ='';
-    }
-    
+//Diposit
+depositBtn.addEventListener('click', ()=>{
+    let inputDipositNumber  = textToNumber(inputDepositText.value);   
+    if(inputDipositNumber > 0){
+        let dInputNumber = textToNumber(inputDepositText.value);
+        let dBalanceNumber = textToNumber(depositBalanceText.innerText);
+        updateBalance(dInputNumber, dBalanceNumber, true);
+        addTable("Deposit", dInputNumber);       
+    }else{
+        alert('error')
+    }       
 })
-
 
 //Withdraw
 withdrawBtn.addEventListener('click', ()=>{
-    let withdrawdBalanceText = parseFloat(withdrawBalance.innerText);
-    let inputWithdrawValue = parseFloat(inputWithdraw.value);
-    let mainBalanceText = parseFloat(mainBalance.innerText);
-
-    if(isNaN(inputWithdraw.value) || inputWithdraw.value === ''){
-        document.getElementById('warning2').innerText = "Please input a vaild number";
+    let inputWithdrawNumber  = textToNumber(inputWithdrawText.value);
+    let mainBalanceNumber = textToNumber(mainBalanceText.innerText);
+    if(inputWithdrawNumber > 0 && inputWithdrawNumber < mainBalanceNumber){
+        let wInputNumber = textToNumber(inputWithdrawText.value);
+    let wBalanceNumber = textToNumber(withdrawBalanceText.innerText);
+    updateBalance(wInputNumber, wBalanceNumber, false);
+    addTable("Withdraw", wInputNumber);
     }else{
-        document.getElementById('warning2').innerText = "";
-         
-        //Nested Condition
-    if(mainBalanceText < inputWithdrawValue){
-        alert('eror');
-        inputWithdraw.value = '';
-    }else{
-        //Add Balance on Deposit Section
-    let totalWithdraw = withdrawdBalanceText + inputWithdrawValue;
-    withdrawBalance.innerText = totalWithdraw.toFixed(2);
-    //Add Balance on Total Balance Section
-        let mainBalanceTotal = mainBalanceText - inputWithdrawValue;
-    mainBalance.innerText = mainBalanceTotal.toFixed(2);
-
-    let tr = document.createElement('tr');
-    let td1 = document.createElement('td');
-    td1.innerText = 'Withdraw'
-    let td2 = document.createElement('td');
-    td2.innerText = 'Success';
-    td2.setAttribute('class', 'status')
-    let td3 = document.createElement('td');
-    td3.innerText = `- $${totalWithdraw.toFixed(2)}`;
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-
-    tableRow.appendChild(tr);
-
-
-    inputDeposit.value ='';
-    }
-    }
+        alert('error')
+    }   
 })

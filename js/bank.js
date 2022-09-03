74216d8fb4e4adcccd57cb6e4631baf6cdf1ddda
Sukanto Das <Suknto01899>
2022-09-03 16:50:49 +0600

    done

--- js/bank.js
+++ js/bank.js
@@ -3,6 +3,9 @@ let inputDeposit = document.getElementById('input-deposit');
 let depositedBalance = document.getElementById('deposit-balance');
 let mainBalance = document.getElementById('main-balance');
 let tableRow = document.querySelector('tbody');
+let withdrawBtn = document.getElementById('withdraw-btn');
+let inputWithdraw = document.getElementById('input-withdraw');
+let withdrawBalance = document.getElementById('withdraw-balance')
 
 document.getElementById('deposit-btn').addEventListener('click', (e)=>{
     let depositedBalanceText = parseFloat(depositedBalance.innerText);
@@ -27,7 +30,7 @@ document.getElementById('deposit-btn').addEventListener('click', (e)=>{
     td2.innerText = 'Success';
     td2.setAttribute('class', 'status')
     let td3 = document.createElement('td');
-    td3.innerText = totalDeposite.toFixed(2);
+    td3.innerText = `+ $${totalDeposite.toFixed(2)}`;
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
@@ -38,4 +41,48 @@ document.getElementById('deposit-btn').addEventListener('click', (e)=>{
     inputDeposit.value ='';
     }
     
+})
+
+
+//Withdraw
+withdrawBtn.addEventListener('click', ()=>{
+    let withdrawdBalanceText = parseFloat(withdrawBalance.innerText);
+    let inputWithdrawValue = parseFloat(inputWithdraw.value);
+    let mainBalanceText = parseFloat(mainBalance.innerText);
+
+    if(isNaN(inputWithdraw.value) || inputWithdraw.value === ''){
+        document.getElementById('warning2').innerText = "Please input a vaild number";
+    }else{
+        document.getElementById('warning2').innerText = "";
+         
+        //Nested Condition
+    if(mainBalanceText < inputWithdrawValue){
+        alert('eror');
+        inputWithdraw.value = '';
+    }else{
+        //Add Balance on Deposit Section
+    let totalWithdraw = withdrawdBalanceText + inputWithdrawValue;
+    withdrawBalance.innerText = totalWithdraw.toFixed(2);
+    //Add Balance on Total Balance Section
+        let mainBalanceTotal = mainBalanceText - inputWithdrawValue;
+    mainBalance.innerText = mainBalanceTotal.toFixed(2);
+
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    td1.innerText = 'Withdraw'
+    let td2 = document.createElement('td');
+    td2.innerText = 'Success';
+    td2.setAttribute('class', 'status')
+    let td3 = document.createElement('td');
+    td3.innerText = `- $${totalWithdraw.toFixed(2)}`;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+
+    tableRow.appendChild(tr);
+
+
+    inputDeposit.value ='';
+    }
+    }
 })
